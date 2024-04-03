0b19f91a160f14a0b5726975e92687fb8909cbc5
vomisa29 <p.mendezm@uniandes.edu.co>
2024-04-02 22:49:37 -0500

    Cambios pequeños

--- caso2_INFRACOMP/src/crearReferencias.java
+++ caso2_INFRACOMP/src/crearReferencias.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.HashMap;
 public class crearReferencias {
     
@@ -25,6 +26,44 @@ public class crearReferencias {
         System.out.println("sizePagina " + sizePagina);
     }
 
+    public void archivoReferencias(){
+        ArrayList<String> lineasArchivo = new ArrayList<String>();
+        matricesToPaginas();//Se llena matrizPagina de información
+
+        //COPIA DEL ALGORITMO
+        //RECUERDA REEMPLAZAR LOS COMENTARIOS CON SETTERS DE R y W
+        for(int i=1;i<nf;i++){
+            for(int j=1;j<nc;j++){
+                int acum = 0;
+                for(int a=-1;a<=1;a++){
+                    for(int b=-1;b<=1;b++){
+                        int i2 = i+a;
+                        int j2 = j+b;
+                        int ie = 1+a;
+                        int je = 1+b;
+                        //acum += (F[i3][j3]*M[i2][j2])
+                    }
+                }
+                if(acum>=0 && acum<=255){
+                    //R[i][j] = acum
+                }else if(acum<0){
+                    //R[i][j] = acum
+                }else{
+                    //R[i][j] = 255
+                }
+            }
+        }
+
+        for(int i=0;i<nc;i++){
+            //R[0][i]=0
+            //R[nf-1][i]=255
+        }
+        for(int i=1;i<nf;i++){
+            //R[i][0]=0
+            //R[i][nc-1]=255
+        }
+    }
+
     public void matricesToPaginas(){
 
         int numIntFiltro = sideFiltro*sideFiltro;
@@ -45,8 +84,8 @@ public class crearReferencias {
                     break;
                 }
                 int[] posMatriz = numAPosMatriz(numIntMatrizActual, tamanioMatrices[k]);
-                String llavePos = nomMatriz[k] + "[" + String.valueOf(posMatriz[0]) +"][" + String.valueOf(posMatriz[1]) + "]";
-                posPagina posInt = new posPagina(i, j);
+                String llavePos = nomMatriz[k] + "[" + String.valueOf(posMatriz[0]) +"][" + String.valueOf(posMatriz[1]) + "]";//posicion del entero en terminos de la matriz correspondiente
+                posPagina posInt = new posPagina(i, j);//posicion del entero en terminos de paginas y desplazamiento
                 
                 this.matrizPagina.put(llavePos,posInt);
 
@@ -69,6 +108,7 @@ public class crearReferencias {
 
     public int[] numAPosMatriz(int num, int[] tamaniosMatriz){
         //Porfavor no modificar, no se como funciona
+        //Toma un número num y lo convierte a una posición i,j en una matriz
         int nf = tamaniosMatriz[0];
         int nc = tamaniosMatriz[1];
         num = (nf*nc+1)-num;
@@ -90,6 +130,8 @@ public class crearReferencias {
         int[] rta = {posi,posj};
         return rta;
     }
+
+
 
     public HashMap<String, posPagina> getMatrizPagina() {
         return matrizPagina;
@@ -139,6 +181,5 @@ public class crearReferencias {
     public void setSizePagina(int sizePagina) {
         this.sizePagina = sizePagina;
     }
-
     
 }
