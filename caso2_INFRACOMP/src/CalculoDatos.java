c8640f78841eef923c23e81008f41a6419c32c6c
RuizL25 <lf.ruizo1@uniandes.edu.co>
2024-04-06 09:34:53 -0500

    V2 mejor adaptada con los cambios del profesor

--- caso2_INFRACOMP/src/CalculoDatos.java
+++ caso2_INFRACOMP/src/CalculoDatos.java
@@ -3,20 +3,17 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-// import java.util.Map;
 
 public class CalculoDatos {
 
     private int miss = 0;
     private int hits = 0;
-    // private int tp;
     private int marcos;
     private String archivo;
     private int numRegistros;
     private double porcentajeHits;
     private HashMap<String, ArrayList<String>> memoriaReal = new HashMap<String, ArrayList<String>>();
     private ArrayList<String> memoriaVirtual = new ArrayList<String>();
-    // private Map<String, String> actualizaciones = new HashMap<>();
 
     public CalculoDatos(int marcos, String archivo) {
         this.marcos = marcos;
@@ -25,12 +22,8 @@ public class CalculoDatos {
 
     public void calcularDatos() {
 
-        // Leer archivo de referencias
-        // HashMap virtual
         llenarListaVirtual();
         llenarHashMapReal();
-        // calcularHits();
-        // calcularFallas();
         leerReferencias();
         System.out.println("Hits: " + this.hits);
         System.out.println("Fallas: " + this.miss);
@@ -46,9 +39,6 @@ public class CalculoDatos {
             BufferedReader br = new BufferedReader(fr);
 
             String referencia;
-
-            // this.tp = Integer.parseInt(br.readLine().substring(3));
-
             for (int i = 1; i < 7; i++) {
                 try {
                     referencia = br.readLine();
@@ -65,28 +55,15 @@ public class CalculoDatos {
             }
 
             br.close();
-            // System.out.println(memoriaVirtual);
-            // System.out.println(memoriaVirtual.size());
-
+            System.out.println(memoriaVirtual);
+            System.out.println(memoriaVirtual.size());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void llenarHashMapReal() {
-        // int memoria = this.marcos * (this.tp / 4);
-        // System.out.println("memoria: " + memoria);
         for (String referencia : this.memoriaVirtual) {
-            // if (contador < memoria) {
-            // String key = referencia.substring(0, 7);
-            // if (!this.memoriaReal.containsKey(key)) {
-            // this.memoriaReal.put(key,
-            // referencia.substring(8).replace(referencia.charAt(referencia.length() - 1),
-            // '1'));
-            // contador++;
-
-            // }
-            // }
             if (Integer.parseInt(referencia.substring(8, 10).replace(",", "")) < marcos) {
                 String key = referencia.substring(8, 10).replace(",", "");
                 if (!this.memoriaReal.containsKey(key)) {
@@ -95,13 +72,6 @@ public class CalculoDatos {
                     // lista.add(referencia);
                     this.memoriaReal.put(key, lista);
                 }
-                // } else {
-                // ArrayList<String> lista = this.memoriaReal.get(key);
-                // if (!lista.contains(referencia)) {
-                // lista.add(referencia);
-                // this.memoriaReal.replace(key, lista);
-                // }
-                // }
             }
 
         }
@@ -130,9 +100,7 @@ public class CalculoDatos {
     }
 
     public void algoritmoLRU(String referencia, Boolean hit) {
-        // String key = referencia.substring(0, 7);
         String pagina = referencia.substring(8, 10).replace(",", "");
-        // String value = referencia.substring(8);
         char operacion = referencia.charAt(referencia.length() - 3);
 
         if (hit) {
@@ -166,64 +134,6 @@ public class CalculoDatos {
 
                 }
             }
-
-            // if (hit) {
-            // char bit = value.charAt(value.length() - 1);
-            // if (bit == '0') {
-            // this.memoriaReal.replace(key, value.substring(0, 5) +
-            // value.substring(5).replace('0', '2'));
-            // } else if (bit == '1') {
-            // this.memoriaReal.replace(key, value.substring(0, 5) +
-            // value.substring(5).replace('0', '3'));
-            // }
-            // } else {
-
-            // Boolean cambio = false;
-            // for (int i = 0; i < 4; i++) {
-            // for (String keyReal : this.memoriaReal.keySet()) {
-            // String valorAnterior = this.memoriaReal.get(keyReal);
-            // char bit = valorAnterior.charAt(valorAnterior.length() - 1);
-            // // Caso cuando hay pagina no referenciadas "0"
-            // if (bit == '0' && i == 0) {
-            // this.memoriaReal.put(key, value.replace(value.charAt(value.length() - 1),
-            // '1'));
-            // this.memoriaReal.remove(keyReal);
-            // System.out.println("HOLA 0");
-            // cambio = true;
-            // break;
-            // }
-            // // Cuando no esta referenciada pero si modificada "1"
-            // else if (bit == '1' && i == 1) {
-            // this.memoriaReal.put(key, value.replace(value.charAt(value.length() - 1),
-            // '1'));
-            // this.memoriaReal.remove(keyReal);
-            // System.out.println("HOLA 1");
-            // cambio = true;
-            // break;
-            // } else if (bit == '2' && i == 2) {
-            // this.memoriaReal.put(key, value.replace(value.charAt(value.length() - 1),
-            // '3'));
-            // this.memoriaReal.remove(keyReal);
-            // System.out.println("HOLA 2");
-            // cambio = true;
-            // break;
-            // } else if (bit == '3' && i == 3) {
-            // this.memoriaReal.put(key, value.replace(value.charAt(value.length() - 1),
-            // '3'));
-            // this.memoriaReal.remove(keyReal);
-            // System.out.println("HOLA 3");
-            // cambio = true;
-            // break;
-
-            // }
-            // }
-
-            // if (cambio) {
-            // break;
-            // }
-            // }
-
-            // }
         } else {
             Boolean cambio = false;
             for (int i = 0; i < 4; i++) {
@@ -296,20 +206,6 @@ public class CalculoDatos {
             }
 
         }
-
-        // public void restart() {
-        // for (String key : this.memoriaReal.keySet()) {
-        // String value = this.memoriaReal.get(key);
-        // if (value.charAt(value.length() - 1) == '3') {
-        // this.memoriaReal.replace(key, value.replace(value.charAt(value.length() - 1),
-        // '1'));
-        // } else if (value.charAt(value.length() - 1) == '2') {
-        // this.memoriaReal.replace(key, value.replace(value.charAt(value.length() - 1),
-        // '0'));
-        // }
-        // }
-        // }
-
     }
 
     public void restart() {
@@ -326,5 +222,4 @@ public class CalculoDatos {
             }
         }
     }
-
 }
